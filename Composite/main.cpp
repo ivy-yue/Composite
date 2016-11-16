92eb1e20b7c00e150506acb0cb0d574ccf7d675a
wangyue <wangyue@macbook-air.local>
2016-11-16 16:36:51 +0800

    Initial Commit

---
+++ Composite/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Composite
+//
+//  Created by wangyue on 2016/11/16.
+//  Copyright © 2016年 ___ivy___. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
